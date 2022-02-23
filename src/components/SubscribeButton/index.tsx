6b533c0491ac25d9d23bfdefdc5f5516860787ca
gabrielleles18 <gabriel.leles18@gmail.com>
2022-02-22 21:42:38 -0300

    Updated

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,29 @@
 import styles from './styles.module.scss';
+import {signin, useSession} from "next-auth/client";
 
 interface SubscribeButtonProps {
     priceId: string
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    function handleSubscribe() {
+        if (!session) {
+            signin('github')
+            return;
+        }
+
+
+
+    }
 
     return (
-        <button type="button" className={styles.signinButton}>
+        <button
+            type="button"
+            className={styles.signinButton}
+            onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     );
